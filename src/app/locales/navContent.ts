fd09e6978a93d5307ffef9a4225232e16a16278a
chensihai230 <csh2302137484@gmail.com>
2026-02-14 12:49:23 +0800

    学员产品

--- src/app/locales/navContent.ts
+++ src/app/locales/navContent.ts
@@ -8,7 +8,7 @@ export const navContent = {
   home: { ZH: '首页', EN: 'Home' },
   platform: { ZH: '学习力平台', EN: 'Learning Platform' },
   system: { ZH: '学习体系', EN: 'Curriculum' },
-  works: { ZH: '学员作品', EN: 'Student Works' },
+  works: { ZH: '学员产品', EN: 'Student Products' },
   philosophy: { ZH: '品牌理念', EN: 'Philosophy' },
   about: { ZH: '关于我们', EN: 'About Us' },
   contact: { ZH: '联系我们', EN: 'Contact' },
